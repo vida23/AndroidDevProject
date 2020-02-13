d565b665d66d9529ea71f66028c68e21f1b61a35
Adam-sturesson <adam99.sturesson@hotmail.se>
2020-02-13 15:42:55 +0100

    Added course material view and a way to get there

---
+++ DIMPGuide/app/src/main/java/com/example/dimpguide/CourseMaterialActivity.kt
@@ -0,0 +1,14 @@
+package com.example.dimpguide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CourseMaterialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_material)
+
+        // Here we need to get a call to the database to fetch all the course material
+    }
+}
--- DIMPGuide/app/src/main/java/com/example/dimpguide/Specific_course.kt
+++ DIMPGuide/app/src/main/java/com/example/dimpguide/Specific_course.kt
@@ -1,7 +1,10 @@
 package com.example.dimpguide
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Specific_course : AppCompatActivity() {
 
@@ -9,6 +12,16 @@ class Specific_course : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_specific_course)
         val name = intent.getStringExtra("name")
+        findViewById<TextView>(R.id.Course).apply{
+            this.text = name
+        }
+        val courseMaterialButton = findViewById<Button>(R.id.CourseMaterialButton)
+
+        courseMaterialButton.setOnClickListener {
+            val intent = Intent(this,CourseMaterialActivity::class.java)
+            //Shall put id from the database here when valid
+            startActivity(intent)
+        }
 
     }
 }
