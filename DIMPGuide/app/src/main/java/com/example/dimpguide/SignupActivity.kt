bdcb404fb2f394aea88cf6b7369bbedfdb25c8f6
Dejan Arsenijevic <arde1712@student.ju.se>
2020-02-26 12:45:01 +0100

    to be continued

--- DIMPGuide/app/src/main/java/com/example/dimpguide/SignupActivity.kt
+++ DIMPGuide/app/src/main/java/com/example/dimpguide/SignupActivity.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_sign_up.*
@@ -18,89 +19,105 @@ class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-                val programs = resources.getStringArray(R.array.Programmes)
-                val years = resources.getStringArray(R.array.Years)
-                val spinnerPrograms = findViewById<Spinner>(R.id.programme)
-                val spinnerYears = findViewById<Spinner>(R.id.year)
-                signUpButton.isEnabled = false
-                if(spinnerPrograms != null){
-                    val spinnerAdapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,programs)
-                    spinnerPrograms.adapter = spinnerAdapter
-                }
-                if(spinnerYears != null){
-                    val spinnerAdapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,years)
-                    spinnerYears.adapter = spinnerAdapter
-                }
-                signUpButton.setOnClickListener{
-                    val docRef = DbHandler.db.collection("users").document()
-                    val chosenYear:String = year.selectedItem.toString()
-                    val chosenYearInt:Int
-                    if(chosenYear == "Year 1"){
-                        chosenYearInt = YEAR_ONE
-                    }else if(chosenYear == "Year 2"){
-                        chosenYearInt = YEAR_TWO
-                    }else{
-                        chosenYearInt = YEAR_THREE
-                    }
-                    docRef.set(User(username = username.text.toString(), password = password.text.toString(), year = chosenYearInt, program = programme.selectedItem.toString()))
-                        .addOnSuccessListener {
-                            val intent = Intent(this,ProgrammeYearActivity::class.java)
-                            startActivity(intent)
-                            LoggedInManager.changeLoginState(state = true)
-                            finish()
-                        }
-                }
-                username.addTextChangedListener(object : TextWatcher{
-                    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                        signUpButton.isEnabled = false
-                    }
+        val programs = resources.getStringArray(R.array.Programmes)
+        val years = resources.getStringArray(R.array.Years)
+        val spinnerPrograms = findViewById<Spinner>(R.id.programme)
+        val spinnerYears = findViewById<Spinner>(R.id.year)
+        signUpButton.isEnabled = false
 
-                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                        if(s!!.length < MIN_USERNAME_LENGTH){
-                            usernameErrorText.text = getString(R.string.toShortUsername)
-                            signUpButton.isEnabled = false
-                        }else{
-                            signUpButton.isEnabled = true
-                            usernameErrorText.text = ""
-                        }
-                    }
 
-                    override fun afterTextChanged(s: Editable?) {
-                        if(password.text.length < MIN_PASSWORD_LENGTH ||  !email.text.contains("@")){
-                    signUpButton.isEnabled = false
-                }
+        if (spinnerPrograms != null) {
+            val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, programs)
+            spinnerPrograms.adapter = spinnerAdapter
+        }
+
+        if (spinnerYears != null) {
+            val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, years)
+            spinnerYears.adapter = spinnerAdapter
+        }
+
+        val username = findViewById<EditText>(R.id.username)
+        val password = findViewById<EditText>(R.id.password)
+        val email = findViewById<EditText>(R.id.email)
+
+        /*signUpButton.setOnClickListener {
+            val docRef = DbHandler.db.collection("users").document()
+            val chosenYear: String = year.selectedItem.toString()
+            val chosenYearInt: Int
+            if (chosenYear == "Year 1") {
+                chosenYearInt = YEAR_ONE
+            } else if (chosenYear == "Year 2") {
+                chosenYearInt = YEAR_TWO
+            } else {
+                chosenYearInt = YEAR_THREE
             }
-        })
-        password.addTextChangedListener(object : TextWatcher{
+            docRef.set(
+                User(
+                    username = username.text.toString(),
+                    password = password.text.toString(),
+                    year = chosenYearInt,
+                    program = programme.selectedItem.toString()
+                )
+            )
+                .addOnSuccessListener {
+                    val intent = Intent(this, ProgrammeYearActivity::class.java)
+                    startActivity(intent)
+                    LoggedInManager.changeLoginState(state = true)
+                    finish()
+                }
+        }
+        username.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 signUpButton.isEnabled = false
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if(s!!.length < MIN_PASSWORD_LENGTH){
+                if (s!!.length < MIN_USERNAME_LENGTH) {
+                    usernameErrorText.text = getString(R.string.toShortUsername)
+                    signUpButton.isEnabled = false
+                } else {
+                    signUpButton.isEnabled = true
+                    usernameErrorText.text = ""
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (password.text.length < MIN_PASSWORD_LENGTH || !email.text.contains("@")) {
+                    signUpButton.isEnabled = false
+                }
+            }
+        })
+        password.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                signUpButton.isEnabled = false
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s!!.length < MIN_PASSWORD_LENGTH) {
                     passwordErrorText.text = getString(R.string.toShortPassword)
                     signUpButton.isEnabled = false
-                }else{
+                } else {
                     signUpButton.isEnabled = true
                     passwordErrorText.text = ""
                 }
             }
 
             override fun afterTextChanged(s: Editable?) {
-                if(username.text.length < MIN_USERNAME_LENGTH || !email.text.contains("@")){
+                if (username.text.length < MIN_USERNAME_LENGTH || !email.text.contains("@")) {
                     signUpButton.isEnabled = false
                 }
             }
         })
-        email.addTextChangedListener(object : TextWatcher{
+        email.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 signUpButton.isEnabled = false
             }
+
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if(!s!!.contains("@")){
+                if (!s!!.contains("@")) {
                     emailErrorText.text = getString(R.string.emailErrorNoAt)
                     signUpButton.isEnabled = false
-                }else{
+                } else {
                     signUpButton.isEnabled = true
                     emailErrorText.text = ""
                 }
@@ -108,14 +125,18 @@ class SignupActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(s: Editable?) {
-                if(username.text.length < MIN_USERNAME_LENGTH || password.text.length < MIN_PASSWORD_LENGTH){
+                if (username.text.length < MIN_USERNAME_LENGTH || password.text.length < MIN_PASSWORD_LENGTH) {
                     signUpButton.isEnabled = false
                 }
             }
-
-        })
+        })*/
     }
-    companion object Companion{
+
+    private fun createAccount() {
+
+    }
+
+    companion object Companion {
         const val MIN_USERNAME_LENGTH = 2
         const val MIN_PASSWORD_LENGTH = 8
         const val YEAR_ONE = 1
