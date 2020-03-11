499f144ccfa8d1a70617d69d8a038197e90a9128
Dejan Arsenijevic <arde1712@student.ju.se>
2020-03-11 19:47:24 +0100

    moved query to function

--- DIMPGuide/app/src/main/java/com/example/dimpguide/ui/home/Courses/CoursesFragment.kt
+++ DIMPGuide/app/src/main/java/com/example/dimpguide/ui/home/Courses/CoursesFragment.kt
@@ -16,6 +16,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.auth.User
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import java.lang.Thread.sleep
 
 class CoursesFragment : Fragment() {
 
@@ -36,14 +37,29 @@ class CoursesFragment : Fragment() {
         lateinit var optionalCourseTwo:String
     }
 
-    private lateinit var viewModel: CoursesViewModel
+    fun setOptionalCourses(){
+        db.collection("users")
+            .whereEqualTo("uid", FirebaseAuth.getInstance().currentUser!!.uid)
+            .get()
+            .addOnSuccessListener { docs ->
+                if (!docs.isEmpty) {
+                    for (doc in docs) {
+                        if (doc.getString("optionalCourseOne").toString() != null) {
+                            optionalCourseOne = doc.getString("optionalCourseOne").toString()
+                            optionalCourseTwo = doc.getString("optionalCourseTwo").toString()
+                        }
+                        Log.i("database", "from user collection " + optionalCourseOne)
+                        Log.i("database", "from user collection " + optionalCourseTwo)
+                    }
+                }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val root = inflater.inflate(R.layout.courses_fragment, container, false)
+            }
+            .addOnFailureListener {
+                Log.i("database", "FAIL")
+            }
+    }
 
+    fun getCourses(){
 
         var chosenProgram = arguments?.getString("Program")
 
@@ -58,159 +74,154 @@ class CoursesFragment : Fragment() {
 
         Log.i("database", chosenProgram.toString())
 
-        db.collection("users")
-            .whereEqualTo("uid", FirebaseAuth.getInstance().currentUser!!.uid)
+        db.collection("courses")
+            .whereIn(
+                "pro",
+                listOf("dimp", chosenProgram)
+            ) //field is either program or dimp
+            .whereEqualTo("year", chosenYear)
+            .orderBy("period", Query.Direction.ASCENDING)
             .get()
-            .addOnSuccessListener { docs ->
-                if (!docs.isEmpty) {
-                    for (doc in docs) {
-                        if (doc.getString("optionalCourseOne").toString() != null) {
-                            optionalCourseOne = doc.getString("optionalCourseOne").toString()
-                            optionalCourseTwo = doc.getString("optionalCourseTwo").toString()
+            .addOnSuccessListener { documents ->
+                // var matchingCourses: String
+                for (document in documents) {
+                    Log.i("database", document.getString("name").toString())
+                    if (document.getLong("period") == 1.toLong()) {
+
+                        if (LOOP_VARIABLE == 0) {
+                            FIRST_COURSE = (document.getString("name")).toString()
+                            FIRST_COURSE_ID = (document.id)
+                            LOOP_VARIABLE += 1
+                            continue
                         }
-                        Log.i("database", "from user collection " + optionalCourseOne)
-                        Log.i("database", "from user collection " + optionalCourseTwo)
+
+                        matchingCourses = (document.getString("name")).toString()
+                        if (FirebaseAuth.getInstance().currentUser != null && document.getString(
+                                "year"
+                            ) == "3" && optionalCourseOne.length > 1
+                        ) {
+                            matchingCourses = optionalCourseOne
+                        }
+
+                        val studyPeriod = StudyPeriod(
+                            FIRST_COURSE,
+                            matchingCourses,
+                            "Period 1",
+                            FIRST_COURSE_ID,
+                            course2_id = document.id,
+                            year = document.getString("year").toString()
+                        )
+                        dataset.add(studyPeriod)
+
+                        LOOP_VARIABLE = 0
+
+                    } else if (document.getLong("period") == 2.toLong()) {
+
+                        if (LOOP_VARIABLE == 0) {
+                            FIRST_COURSE = (document.getString("name")).toString()
+                            FIRST_COURSE_ID = (document.id)
+                            LOOP_VARIABLE += 1
+                            continue
+                        }
+                        matchingCourses = (document.getString("name")).toString()
+                        if (FirebaseAuth.getInstance().currentUser != null && document.getString(
+                                "year"
+                            ) == "3" && optionalCourseTwo.length > 1
+                        ) {
+                            matchingCourses = optionalCourseTwo
+                        }
+
+                        val studyPeriod = StudyPeriod(
+                            FIRST_COURSE,
+                            matchingCourses,
+                            "Period 2",
+                            FIRST_COURSE_ID,
+                            course2_id = document.id,
+                            year = document.getString("year").toString()
+                        )
+                        dataset.add(studyPeriod)
+
+                        LOOP_VARIABLE = 0
+
+                    } else if (document.getLong("period") == 3.toLong()) {
+                        Log.i("database", document.getString("name"))
+                        if (LOOP_VARIABLE == 0) {
+                            FIRST_COURSE = (document.getString("name")).toString()
+                            FIRST_COURSE_ID = (document.id)
+                            LOOP_VARIABLE += 1
+                            continue
+                        }
+                        matchingCourses = (document.getString("name")).toString()
+
+                        val studyPeriod = StudyPeriod(
+                            FIRST_COURSE,
+                            matchingCourses,
+                            "Period 3",
+                            FIRST_COURSE_ID,
+                            course2_id = document.id,
+                            year = document.getString("year").toString()
+                        )
+                        dataset.add(studyPeriod)
+
+                        LOOP_VARIABLE = 0
+
+                    } else if (document.getLong("period") == 4.toLong()) {
+                        Log.i("database", document.getString("name"))
+                        if (LOOP_VARIABLE == 0) {
+                            FIRST_COURSE = (document.getString("name")).toString()
+                            FIRST_COURSE_ID = (document.id)
+                            LOOP_VARIABLE += 1
+                            continue
+                        }
+
+                        matchingCourses = (document.getString("name")).toString()
+
+                        val studyPeriod = StudyPeriod(
+                            FIRST_COURSE,
+                            matchingCourses,
+                            "Period 4",
+                            FIRST_COURSE_ID,
+                            course2_id = document.id,
+                            year = document.getString("year").toString()
+                        )
+                        dataset.add(studyPeriod)
+
+                        LOOP_VARIABLE = 0
                     }
 
-                    db.collection("courses")
-                        .whereIn(
-                            "pro",
-                            listOf("dimp", chosenProgram)
-                        ) //field is either program or dimp
-                        .whereEqualTo("year", chosenYear)
-                        .orderBy("period", Query.Direction.ASCENDING)
-                        .get()
-                        .addOnSuccessListener { documents ->
-                            // var matchingCourses: String
-                            for (document in documents) {
-                                Log.i("database", document.getString("name").toString())
-                                if (document.getLong("period") == 1.toLong()) {
-
-                                    if (LOOP_VARIABLE == 0) {
-                                        FIRST_COURSE = (document.getString("name")).toString()
-                                        FIRST_COURSE_ID = (document.id)
-                                        LOOP_VARIABLE += 1
-                                        continue
-                                    }
-
-                                    matchingCourses = (document.getString("name")).toString()
-                                    if (FirebaseAuth.getInstance().currentUser != null && document.getString(
-                                            "year"
-                                        ) == "3" && optionalCourseOne.length > 1
-                                    ) {
-                                        matchingCourses = optionalCourseOne
-                                    }
-
-                                    val studyPeriod = StudyPeriod(
-                                        FIRST_COURSE,
-                                        matchingCourses,
-                                        "Period 1",
-                                        FIRST_COURSE_ID,
-                                        course2_id = document.id,
-                                        year = document.getString("year").toString()
-                                    )
-                                    dataset.add(studyPeriod)
-
-                                    LOOP_VARIABLE = 0
-
-                                } else if (document.getLong("period") == 2.toLong()) {
-
-                                    if (LOOP_VARIABLE == 0) {
-                                        FIRST_COURSE = (document.getString("name")).toString()
-                                        FIRST_COURSE_ID = (document.id)
-                                        LOOP_VARIABLE += 1
-                                        continue
-                                    }
-                                    matchingCourses = (document.getString("name")).toString()
-                                    if (FirebaseAuth.getInstance().currentUser != null && document.getString(
-                                            "year"
-                                        ) == "3" && optionalCourseTwo.length > 1
-                                    ) {
-                                        matchingCourses = optionalCourseTwo
-                                    }
-
-                                    val studyPeriod = StudyPeriod(
-                                        FIRST_COURSE,
-                                        matchingCourses,
-                                        "Period 2",
-                                        FIRST_COURSE_ID,
-                                        course2_id = document.id,
-                                        year = document.getString("year").toString()
-                                    )
-                                    dataset.add(studyPeriod)
-
-                                    LOOP_VARIABLE = 0
-
-                                } else if (document.getLong("period") == 3.toLong()) {
-                                    Log.i("database", document.getString("name"))
-                                    if (LOOP_VARIABLE == 0) {
-                                        FIRST_COURSE = (document.getString("name")).toString()
-                                        FIRST_COURSE_ID = (document.id)
-                                        LOOP_VARIABLE += 1
-                                        continue
-                                    }
-                                    matchingCourses = (document.getString("name")).toString()
-
-                                    val studyPeriod = StudyPeriod(
-                                        FIRST_COURSE,
-                                        matchingCourses,
-                                        "Period 3",
-                                        FIRST_COURSE_ID,
-                                        course2_id = document.id,
-                                        year = document.getString("year").toString()
-                                    )
-                                    dataset.add(studyPeriod)
-
-                                    LOOP_VARIABLE = 0
-
-                                } else if (document.getLong("period") == 4.toLong()) {
-                                    Log.i("database", document.getString("name"))
-                                    if (LOOP_VARIABLE == 0) {
-                                        FIRST_COURSE = (document.getString("name")).toString()
-                                        FIRST_COURSE_ID = (document.id)
-                                        LOOP_VARIABLE += 1
-                                        continue
-                                    }
-
-                                    matchingCourses = (document.getString("name")).toString()
-
-                                    val studyPeriod = StudyPeriod(
-                                        FIRST_COURSE,
-                                        matchingCourses,
-                                        "Period 4",
-                                        FIRST_COURSE_ID,
-                                        course2_id = document.id,
-                                        year = document.getString("year").toString()
-                                    )
-                                    dataset.add(studyPeriod)
-
-                                    LOOP_VARIABLE = 0
-                                }
-
-                            }
-                            viewManager = LinearLayoutManager(context)
-                            viewAdapter = CourseRecyclerViewAdapter(dataset, context!!)
-
-                            coursesRecyclerView =
-                                root.findViewById<RecyclerView>(R.id.coursesRecyclerView).apply {
-                                    setHasFixedSize(true)
-
-                                    layoutManager = viewManager
-
-                                    adapter = viewAdapter
-                                }
-                        }
-
-                        .addOnFailureListener { exception ->
-                            Log.w("Could not find", "Error getting documents: ", exception)
-                        }
-
                 }
+                viewManager = LinearLayoutManager(context)
+                viewAdapter = CourseRecyclerViewAdapter(dataset, context!!)
 
+                coursesRecyclerView =
+                    view!!.findViewById<RecyclerView>(R.id.coursesRecyclerView).apply {
+                        setHasFixedSize(true)
+
+                        layoutManager = viewManager
+
+                        adapter = viewAdapter
+                    }
             }
-            .addOnFailureListener {
-                Log.i("database", "FAIL")
+
+            .addOnFailureListener { exception ->
+                Log.w("Could not find", "Error getting documents: ", exception)
             }
+    }
+    private lateinit var viewModel: CoursesViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.courses_fragment, container, false)
+
+        if (FirebaseAuth.getInstance().currentUser != null){
+            setOptionalCourses()
+            sleep(200)
+            getCourses()
+        }else{
+            getCourses()
+        }
 
         return root
     }
